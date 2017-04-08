db596b47dc7b85dce082ef6ec36d1bfa9b5223bc
Brandon Reid <brandonreid2@my.unt.edu>
2017-04-08 15:12:21 -0500

    Create main.cpp

---
+++ Homework3_Program/main.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : main.cpp
+// Author      : Brandon A. Reid
+// Version     : V.1
+// Copyright   : 2016
+// Description : main.cpp for homework3 program, refer to inputData() in func.cpp
+// ============================================================================
+
+#include "H3_H.h"
+
+int main()
+{
+	NodePtr list = nullptr;
+
+	cmds();
+
+	inputData(list); 
+
+	return 0;
+}
+
